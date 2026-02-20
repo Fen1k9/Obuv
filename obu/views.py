f15eba3841a46b81178ea14acae72bbb4956485c
student-pro <student-pro@b301-5.khpk.ru>
2026-02-20 14:54:34 +0700

    Search/sort

--- obu/views.py
+++ obu/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 from .models import User, Product
+from  django.db.models import  Q
 
 def login_user(request):
     if request.method == 'POST':
@@ -30,23 +31,85 @@ def login_user(request):
             return render(request, 'aut.html', {'error': 'Пользователь не найден'})
     return render(request, 'aut.html')
 
+
 def admin_page(request):
-    products = Product.objects.all().order_by('id')
+
+    query = request.GET.get('q', '')
+    sort_by = request.GET.get('sort', 'default')
+
+
+    # Базовый запрос всех товаров
+    products = Product.objects.all().select_related('category', 'producer', 'provider').order_by('id')
+
+    if sort_by == 'stock_asc':
+        products = products.order_by('stock_quantity')
+    elif sort_by == 'stock_desc':
+        products = products.order_by('-stock_quantity')
+    else:
+        products = products.order_by('id')
+
+    if query:
+        products = products.filter(
+            Q(article__icontains=query) |
+            Q(product_name__icontains=query) |
+            Q(description__icontains=query) |
+            Q(category__category_name__icontains=query) |
+            Q(producer__producer_name__icontains=query) |
+            Q(provider__provider_name__icontains=query)
+        )
+
+
     for prod in products:
         if prod.discount_percent > 0:
             prod.discounted_price = prod.price * (1 - prod.discount_percent / 100)
         else:
             prod.discounted_price = prod.price
-    return render(request, 'admin.html', {'products': products})
+
+    context = {
+        'products': products,
+        'query': query,  # передаем поисковый запрос в шаблон
+    }
+    return render(request, 'admin.html', context)
+
 
 def manager_page(request):
-    products = Product.objects.all().order_by('id')
+
+    query = request.GET.get('q', '')
+    sort_by = request.GET.get('sort', 'default')
+
+    products = Product.objects.all().select_related('category', 'producer', 'provider').order_by('id')
+
+    if sort_by == 'stock_asc':
+        products = products.order_by('stock_quantity')
+    elif sort_by == 'stock_desc':
+        products = products.order_by('-stock_quantity')
+    else:
+        products = products.order_by('id')
+
+    # Если есть поисковый запрос, фильтруем товары
+    if query:
+        products = products.filter(
+            Q(article__icontains=query) |
+            Q(product_name__icontains=query) |
+            Q(description__icontains=query) |
+            Q(category__category_name__icontains=query) |
+            Q(producer__producer_name__icontains=query) |
+            Q(provider__provider_name__icontains=query)
+        )
+
+
+
     for prod in products:
         if prod.discount_percent > 0:
             prod.discounted_price = prod.price * (1 - prod.discount_percent / 100)
         else:
             prod.discounted_price = prod.price
-    return render(request, 'manager.html', {'products': products})
+
+    context = {
+        'products': products,
+        'query': query,
+    }
+    return render(request, 'manager.html', context)
 
 def client_page(request):
     products = Product.objects.all().order_by('id')
